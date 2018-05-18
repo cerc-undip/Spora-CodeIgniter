4bb251a8986df69033a3707ad899ab9dd3c88433
titoanugerah <titoanugerah@student.undip.ac.id>
2018-05-18 13:52:19 +0700

    Index Done

--- apz/views/temp.php
+++ apz/views/temp.php
@@ -18,15 +18,16 @@
 	<div class="super_container">
 	<!-- Header -->
 	<header class="header trans_300">
-		<div class="top_nav">
-        </div>
-        
+		<!-- Top Navigation -->
+
+		<!-- Main Navigation -->
+
 		<div class="main_nav_container">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12 text-right">
 						<div class="logo_container">
-							<img src="<?= base_url();?>assets/img/logo.png" style="">
+							<img src="<?php echo base_url();?>assets/img/logo.png" style="">
 						</div>
 						<nav class="navbar">
 							<ul class="navbar_menu">
@@ -34,14 +35,10 @@
 								<li><a href="#">Spora Market</a></li>
 								<li><a href="#">Spora Phyte</a></li>
 								<li><a href="contact.html">Tentang</a></li>
-                                <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-                                <li class="account">
-                                    <a href="#"><i class="fa fa-user" style="min-width:30px"></i>&nbsp; Account</a>
-                                    <ul class="account_selection">
-										<li><a href="#"><i class="fa fa-sign-in"></i>Sign In</a></li>
-										<li><a href="#"><i class="fa fa-user-plus"></i>Register</a></li>
-									</ul>
-                                </li>
+							</ul>
+							<ul class="navbar_user">
+								<li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+								<li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
 							</ul>
 							<div class="hamburger_container">
 								<i class="fa fa-bars" aria-hidden="true"></i>
@@ -75,14 +72,12 @@
 
 	<!-- Slider -->
 
-	<div class="main_slider" style="background-image:url(<?php echo base_url(); ?>assets/img/carousel1.jpg)">
+	<div class="main_slider" style="background-image:url(<?php echo base_url(); ?>assets/img/carousel1.jpg); margin-top: 50px;">
 		<div class="container fill_height">
 			<div class="row align-items-center fill_height">
 				<div class="col">
 					<div class="main_slider_content">
-						<h6>Spring / Summer Collection 2017</h6>
-						<h1>Get up to 30% Off New Arrivals</h1>
-						<div class="red_button shop_now_button"><a href="#">shop now</a></div>
+
 					</div>
 				</div>
 			</div>
@@ -90,34 +85,6 @@
 	</div>
 
 	<!-- Banner -->
-
-	<div class="banner">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="banner_item align-items-center" style="background-image:url(images/banner_1.jpg)">
-						<div class="banner_category">
-							<a href="categories.html">women's</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="banner_item align-items-center" style="background-image:url(images/banner_2.jpg)">
-						<div class="banner_category">
-							<a href="categories.html">accessories's</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="banner_item align-items-center" style="background-image:url(images/banner_3.jpg)">
-						<div class="banner_category">
-							<a href="categories.html">men's</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 
 	<!-- New Arrivals -->
 
@@ -323,7 +290,7 @@
 			<div class="row align-items-center">
 				<div class="col-lg-6">
 					<div class="deal_ofthe_week_img">
-						<img src="<?php echo base_url();?>assets/img/carousel2.jpg" alt="">
+						<img src="<?php echo base_url();?>assets/img/backgroundPromo-01.jpg" alt="">
 					</div>
 				</div>
 				<div class="col-lg-6 text-right deal_ofthe_week_col">
@@ -655,26 +622,6 @@
 
 	<!-- Newsletter -->
 
-	<div class="newsletter">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="newsletter_text d-flex flex-column justify-content-center align-items-lg-start align-items-md-center text-center">
-						<h4>Newsletter</h4>
-						<p>Subscribe to our newsletter and get 20% off your first purchase</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<form action="post">
-						<div class="newsletter_form d-flex flex-md-row flex-column flex-xs-column align-items-center justify-content-lg-end justify-content-center">
-							<input id="newsletter_email" type="email" placeholder="Your email" required="required" data-error="Valid email is required.">
-							<button id="newsletter_submit" type="submit" class="newsletter_submit_btn trans_300" value="Submit">subscribe</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
 
 	<!-- Footer -->
 
@@ -705,7 +652,7 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="footer_nav_container">
-						<div class="cr">©2018 All Rights Reserverd. This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a></div>
+						<div class="cr">© <?php echo date('Y'); ?> All Rights Reserverd. This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a></div>
 					</div>
 				</div>
 			</div>
