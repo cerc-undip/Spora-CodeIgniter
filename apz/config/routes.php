ccd2ec90d5376719b63bfab792bea9f96819ef02
vizyan <vizyan.1@gmail.com>
2018-05-18 09:04:09 +0700

    eg login

--- apz/config/routes.php
+++ apz/config/routes.php
@@ -5,4 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+//LOGIN
 $route['login/(:any)'] = 'login/view/$1';
+$route['login/login'] = 'login/login';
--- apz/controllers/Login.php
+++ apz/controllers/Login.php
@@ -3,12 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('login/login_user');
-	}
-    
     public function view($parameter){
         $this->load->view('login/'.$parameter);
     }
+    
+    public function login(){
+        $email = $this->$input->post('etEmail');
+        $password = $this->$input->post('etPassword');
+    }
+        
 }
