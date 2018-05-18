38c964841cd9579f53b666e6c6c02f9edab9a171
fannyhasbi <fnyhsbi@gmail.com>
2018-05-19 02:00:16 +0700

    I prefer conflict

--- apz/controllers/User.php
+++ apz/controllers/User.php
@@ -178,6 +178,7 @@ class User extends CI_Controller {
         }
         else if($this->input->post('ubah_pass')){
             $user = $this->user_model->getUser($this->session->userdata('email'));
+<<<<<<< Updated upstream
 
             if(password_verify($this->input->post('old-password'), $user->password)){
                 if($this->input->post('password') == $this->input->post('password2')){
@@ -197,6 +198,27 @@ class User extends CI_Controller {
                 $this->session->set_flashdata('msg', 'Password salah.');
             }
 
+=======
+
+            if(password_verify($this->input->post('old-password'), $user->password)){
+                if($this->input->post('password') == $this->input->post('password2')){
+                    $this->user_model->updatePass();
+                    $this->session->set_flashdata('type', 'success');
+                    $this->session->set_flashdata('msg', 'Password berhasil diganti.');
+                }
+                else {
+                    // password tidak sama
+                    $this->session->set_flashdata('type', 'warning');
+                    $this->session->set_flashdata('msg', 'Password tidak cocok.');
+                }
+            }
+            else {
+                // Password lama salah
+                $this->session->set_flashdata('type', 'warning');
+                $this->session->set_flashdata('msg', 'Password salah.');
+            }
+
+>>>>>>> Stashed changes
             redirect(site_url('profile'));
         }
         else {
--- apz/views/dashboard/user/main.php
+++ apz/views/dashboard/user/main.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<<<<<<< Updated upstream
     <head>
         <title>Spora</title>
         <meta charset="utf-8">
@@ -19,6 +20,27 @@
         <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/coloshop/categories_responsive.css">
         <script src="<?php echo base_url(); ?>assets/js/coloshop/jquery-3.2.1.min.js"></script>
     </head>
+=======
+<head>
+  <title>Spora</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Colo Shop Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/coloshop/bootstrap4/bootstrap.min.css">
+  <link href="<?= base_url() ?>assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/plugins/OwlCarousel2-2.2.1/animate.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/coloshop/main_styles.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/custom.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/coloshop/responsive.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/plugins/jquery-ui-1.12.1.custom/jquery-ui.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/coloshop/categories_styles.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/coloshop/categories_responsive.css">
+  <script src="<?php echo base_url(); ?>assets/js/coloshop/jquery-3.2.1.min.js"></script>
+</head>
+>>>>>>> Stashed changes
 <body>
 	<div class="super_container">
 <<<<<<< Updated upstream
